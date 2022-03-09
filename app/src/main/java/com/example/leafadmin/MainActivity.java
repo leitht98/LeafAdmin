d94b465b4b9b9ab9c7e500a2eb5b64b546a00aff
thomas <thomasjleith@gmail.com>
2022-03-09 14:27:52 +0000

    Improved sign in/ out interface

--- app/src/main/java/com/example/leafadmin/MainActivity.java
+++ app/src/main/java/com/example/leafadmin/MainActivity.java
@@ -7,14 +7,18 @@ import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     SignInButton btnSignIn;
+    Button signOutButton;
     private GoogleSignInClient mGoogleSignInClient;
     public static final int RC_SIGN_IN = 123;
     private FirebaseAuth mAuth;
@@ -47,25 +52,8 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         requestGoogleSignIn();
 
-        Button signOutButton = findViewById(R.id.signOutButton);
-
-        //This doesn't seem to work, let's try it another time.
-        /*
-        ExecutorService coveringsService = Executors.newSingleThreadExecutor();
-        Future<ArrayList<Covering>> future = coveringsService.submit(new getCoveringsAsFuture());
-        try{
-            future.get();
-            Toast.makeText(MainActivity.this, "!!!!!!!!!", Toast.LENGTH_SHORT).show();
-            coveringsObjectArray.add(new Covering("a",BigDecimal.valueOf(3),BigDecimal.valueOf(4)));
-            //Toast.makeText(MainActivity.this, coveringsObjectArray.size(), Toast.LENGTH_SHORT).show();
-            for(Covering i: coveringsObjectArray){
-                Toast.makeText(MainActivity.this, i.getCoveringName(), Toast.LENGTH_SHORT).show();
-            }
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
+        signOutButton = findViewById(R.id.signOutButton);
+        //signOutButton.setVisibility(View.GONE);
 
         addCovering.setOnClickListener(v -> {
             if(isNetworkAvailable()){
@@ -79,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                                     dataString.append(", id=").append(document.getId()).append("}");
                                 }
                                 openCoveringActivity(dataString.toString());
-                            } else {Toast.makeText(MainActivity.this, "Please connect to the internet.", Toast.LENGTH_SHORT).show();}
+                            } else {Toast.makeText(MainActivity.this, "Please sign in.", Toast.LENGTH_SHORT).show();}
                         });
             } else{Toast.makeText(MainActivity.this, "Please connect to the internet.", Toast.LENGTH_SHORT).show();}
         });
@@ -95,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
                                     dataString.append(", id=").append(document.getId()).append("}");
                                 }
                                 openPesticideActivity(dataString.toString());
-                            } else {Toast.makeText(MainActivity.this, "Please connect to the internet.", Toast.LENGTH_SHORT).show();}
+                            } else {Toast.makeText(MainActivity.this, "Please sign in.", Toast.LENGTH_SHORT).show();}
                         });
             } else{Toast.makeText(MainActivity.this, "Please connect to the internet.", Toast.LENGTH_SHORT).show();}
         });
@@ -111,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
                                     dataString.append(", id=").append(document.getId()).append("}");
                                 }
                                 openViewCoveringsActivity(dataString.toString());
-                            } else {Toast.makeText(MainActivity.this, "Please connect to the internet.", Toast.LENGTH_SHORT).show();}
+                            } else {Toast.makeText(MainActivity.this, "Please sign in.", Toast.LENGTH_SHORT).show();}
                         });
             } else{Toast.makeText(MainActivity.this, "Please connect to the internet.", Toast.LENGTH_SHORT).show();}
         });
@@ -127,16 +115,25 @@ public class MainActivity extends AppCompatActivity {
                                     dataString.append(", id=").append(document.getId()).append("}");
                                 }
                                 openViewPesticidesActivity(dataString.toString());
-                            } else {Toast.makeText(MainActivity.this, "Please connect to the internet.", Toast.LENGTH_SHORT).show();}
+                            } else {Toast.makeText(MainActivity.this, "Please sign in.", Toast.LENGTH_SHORT).show();}
                         });
             } else{Toast.makeText(MainActivity.this, "Please connect to the internet.", Toast.LENGTH_SHORT).show();}
         });
 
-        btnSignIn.setOnClickListener(v -> signIn());
+        btnSignIn.setOnClickListener(v -> {
+            if(isNetworkAvailable()){
+                signIn();
+                signOutButton.setVisibility(View.VISIBLE);
+                btnSignIn.setVisibility(View.GONE);
+            } else {Toast.makeText(MainActivity.this, "Please connect to the internet.", Toast.LENGTH_SHORT).show();}
+        });
 
         signOutButton.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
+            mGoogleSignInClient.revokeAccess();
             Toast.makeText(MainActivity.this, "Signed out.", Toast.LENGTH_SHORT).show();
+            signOutButton.setVisibility(View.GONE);
+            btnSignIn.setVisibility(View.VISIBLE);
         });
     }
 
@@ -144,9 +141,17 @@ public class MainActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        try {FirebaseUser currentUser = mAuth.getCurrentUser();
-        } catch (Exception e){Toast.makeText(MainActivity.this, "Welcome, please sign in to continue", Toast.LENGTH_SHORT).show();}
-
+        try {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            //Toast.makeText(MainActivity.this, "Welcome back "+currentUser.getDisplayName(), Toast.LENGTH_SHORT).show();
+            currentUser.getDisplayName();
+            signOutButton.setVisibility(View.VISIBLE);
+            btnSignIn.setVisibility(View.GONE);
+        } catch (Exception e){
+            //Toast.makeText(MainActivity.this, "Welcome, please sign in to continue", Toast.LENGTH_SHORT).show();
+            signOutButton.setVisibility(View.GONE);
+            btnSignIn.setVisibility(View.VISIBLE);
+        }
     }
 
     public void openCoveringActivity(String dataString){
@@ -179,59 +184,12 @@ public class MainActivity extends AppCompatActivity {
         return networkInfo != null && networkInfo.isConnected();
     }
 
-    /*
-    class getCoveringsAsFuture implements Callable<ArrayList<Covering>>{
-
-        @Override
-        public ArrayList<Covering> call() throws Exception {
-            ArrayList<Covering> tempCoveringArray = new ArrayList<>();
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-            if(connectivityManager.getActiveNetworkInfo().getState() == NetworkInfo.State.CONNECTED ||
-                    connectivityManager.getActiveNetworkInfo().getState() == NetworkInfo.State.CONNECTED) {
-                db.collection("coverings")
-                        .get()
-                        .addOnCompleteListener(task -> {
-                            StringBuilder dataString = new StringBuilder();
-                            if (task.isSuccessful()) {
-                                for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
-                                    dataString.append(document.getData());
-                                }
-                                String dataStringTrim = dataString.substring(1,dataString.length()-1);
-                                String[] coveringsData = dataStringTrim.split("\\}\\{");
-                                for(String i : coveringsData){
-                                    //System.out.println("individual data: "+i);
-                                    String[] coveringValues = i.split(",");
-                                    System.out.println("UV Rate: "+coveringValues[0].split("=")[1]+"\nUV Fen: "+coveringValues[1].split("=")[1]+"\nName: "+coveringValues[2].split("=")[1]);
-                                    BigDecimal rate = new BigDecimal(coveringValues[0].split("=")[1]);
-                                    BigDecimal fen = new BigDecimal(coveringValues[1].split("=")[1]);
-                                    String name = coveringValues[2].split("=")[1];
-                                    //I'm not sure this works? Like might be useless.
-                                    boolean newCovering = true;
-                                    for (Covering j: coveringsObjectArray){
-                                        if(j.getCoveringName().equals(name)){
-                                            newCovering = false;
-                                        }
-                                    }
-                                    if(newCovering) {
-                                        coveringsObjectArray.add(new Covering(id,name,fen,rate));
-                                    }
-                                }
-                            } else {
-                                System.out.println("Fail at check 1");
-                            }
-                        });
-            } else{
-                System.out.println("Fail at check 2");
-            }
-            return tempCoveringArray;
-        }
-    }*/
-
     private void requestGoogleSignIn(){
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                //.requestIdToken(getString(R.string.default_web_client_id))
+                //This seems to work, but I like the old one. I just hate the error message
+                .requestIdToken("794829267701-bba4q60202usjbbkk7hfldd3v24n60p6.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
